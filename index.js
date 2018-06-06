db8b8a52a2e94eb3d1d70476543ee9907263bd09
Denise Pendleton <dpendl1@hotmail.com>
2018-06-06 19:16:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function showRepositories(event, data){
   var repos = JSON.parse(this.responseText)
   console.log("data:", data)
   console.log("event", event);
+  console.log(event === this);
   // console.log(repos)
   // const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
